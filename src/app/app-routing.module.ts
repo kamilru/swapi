14baf465c9aaa87596292e9c62bd95546753d730
Kamil Rusnarczyk <38756027+kamilru@users.noreply.github.com>
2023-10-14 18:43:35 +0200

    create routing and set styles for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/game/game.module').then(m => m.GameModule)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
