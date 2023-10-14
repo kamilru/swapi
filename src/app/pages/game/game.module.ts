1fc1759b2644ae9cf301bf8eeb6d3d53facc1160
Kamil Rusnarczyk <38756027+kamilru@users.noreply.github.com>
2023-10-14 19:44:28 +0200

    create card component

--- src/app/pages/game/game.module.ts
+++ src/app/pages/game/game.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameComponent } from './game.component';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import {MatCardModule} from '@angular/material/card';
 
 const routes: Routes = [
   {
@@ -12,11 +14,13 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    GameComponent
+    GameComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatCardModule
   ]
 })
 export class GameModule { }
